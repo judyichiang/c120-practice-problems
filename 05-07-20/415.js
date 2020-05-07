3c2135c7fcfdb363673e4a7ce1c6837dd4a327c7
Judy Chiang <jiyc1119@gmail.com>
2020-05-07 14:44:03 -0700

    update

---
+++ 05-07-20/415.js
@@ -0,0 +1,33 @@
+// leetcode #415
+// Given two non-negative integers num1 and num2 represented as string, return the sum of num1 and num2.
+
+// Note:
+
+// The length of both num1 and num2 is < 5100.
+// Both num1 and num2 contains only digits 0-9.
+// Both num1 and num2 does not contain any leading zero.
+// You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+
+//convert parameter into Number
+//return the number via string coersion
+//the end
+
+
+var num1 = "0"
+var num2 = "0"
+var addStrings = function (num1, num2) {
+  let result = Number(num1) + Number(num2)
+  // return ""+ result
+  return result.toString()
+}
+
+addStrings(num1, num2)
+
+// Challenger Blake / Jack
+// Completed by Judy
---
+++ 05-07-20/unionArrays.js
@@ -0,0 +1,54 @@
+// Union Arrays
+// Challenge: Given two arrays, create one array with all of the values of both and remove the duplicates.
+
+var arr1 = [1, 2, 3, 4, 5, 3, 2, 4, 8];
+var arr2 = [4, 5, 6, 7, 8];
+
+// function unionArr(arr1, arr2) {
+//   let sortedArr = arr1.concat(arr2).sort();
+//   if (sortedArr.some((val, i) => sortedArr.indexOf(val) !== i)) return
+//   for (let i = 0; i < sortedArr.length; i++) {
+//     for (let j = 1; i > j;j++) {
+//       if (sortedArr[i] === sortedArr[j]) {
+//         sortedArr.splice(i, 1)
+//       }
+//     }
+//   }
+//   return sortedArr
+// }
+// unionArr(arr1, arr2);
+
+var arr1 = [1, 2, 3, 4, 5, 3, 2, 4, 8];
+var arr2 = [4, 5, 6, 7, 8];
+
+function unionArrays(arr1, arr2) {
+  var mergedArray = arr1.concat(arr2);
+
+  var outputArray = [];
+
+  for (var i = 0; i < mergedArray.length; i++) {
+    var isUnique = false;
+    var currNum = mergedArray[i];
+
+    if (i == 0) {
+      outputArray.push(currNum);
+    }
+    else {
+      for (var o = 0; o < outputArray.length; o++) {
+        var outNum = outputArray[o];
+        if (currNum === outNum) {
+          isUnique = false;
+          break;
+        }
+        else {
+          isUnique = true;
+        }
+      }
+    }
+
+    if (isUnique) {
+      outputArray.push(currNum);
+    }
+  }
+  return outputArray;
+}
